b35526f9f222a074f3e6318f8c29564183a4093b
jkloris <kapusta.jergus@gmail.com>
2024-05-11 16:58:43 +0200

    Add mongodb compose file

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -10,10 +10,24 @@ $ProjectRoot = "${PSScriptRoot}/.."
 
 $env:XKAPUSTAJ_API_ENVIRONMENT="Development"
 $env:XKAPUSTAJ_API_PORT="8086"
+$env:AMBULANCE_API_MONGODB_USERNAME="root"
+$env:AMBULANCE_API_MONGODB_PASSWORD="neUhaDnes"
+
+function mongo {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
 
 switch ($command) {
     "start" {
-        go run ${ProjectRoot}/cmd/xkapustaj-api-service
+        try {
+            mongo up --detach
+            go run ${ProjectRoot}/cmd/xkapustaj-api-service
+        } finally {
+            mongo down
+        }
+    }
+    "mongo" {
+        mongo up
     }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
